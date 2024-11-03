626eee2f5a2160620c3387b9443112dd9251787a
JacobWick <lekonhre@gmail.com>
2024-11-03 20:23:42 +0100

    Login i Register endpoints

---
+++ PolyBoard.Server/PolyBoard.Server.Application/Users/Commands/RegisterUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace PolyBoard.Server.Application.Users.Commands;
+
+public class RegisterUserCommand : IRequest<bool>
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ PolyBoard.Server/PolyBoard.Server.Application/Users/CommandsHandlers/LoginUserCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using PolyBoard.Server.Application.Users.Commands;
+using PolyBoard.Server.Core.Entities;
+
+namespace PolyBoard.Server.Application.Users.CommandsHandlers;
+
+public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, bool>
+{
+    private readonly UserManager<User> _userManager;
+
+    public LoginUserCommandHandler(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<bool> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userManager.FindByNameAsync(request.Username);
+        if (user == null) return false;
+
+        return await _userManager.CheckPasswordAsync(user, request.Password);
+    }
+}
+
