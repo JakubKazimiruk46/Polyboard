b91c40ca1ef8e15fafcbbd7f6d1b72ed71e2e998
Łukasz Halicki <85924@student.pb.edu.pl>
2024-11-08 18:21:22 +0100

    Fixed warnings + docker compose sketch

---
+++ PolyBoard.Server/PolyBoard.Server.Core/Entities/Cards/GoToJailCard.cs
@@ -0,0 +1,12 @@
+using PolyBoard.Server.Core.Helpers;
+
+namespace PolyBoard.Server.Core.Entities.Cards;
+
+public sealed class GoToJailCard : Card
+{
+
+    public override void ApplyEffect(Player player)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ PolyBoard.Server/PolyBoard.Server.Core/Helpers/UserConnection.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace PolyBoard.Server.Core.Helpers;
+
+[NotMapped]
+public class UserConnection
+{
+    public string Username { get; set; } = string.Empty;
+
+    public string GameRoom { get; set; } = string.Empty;
+}
