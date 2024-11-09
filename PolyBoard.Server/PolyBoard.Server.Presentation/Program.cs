c1af1f427bcb880181ce72d3b99579159561e883
Łukasz Halicki <lukhal@netwise.pl>
2024-11-09 18:29:34 +0100

    updated configuration, DI and docker-compose

--- PolyBoard.Server/PolyBoard.Server.Presentation/Program.cs
+++ PolyBoard.Server/PolyBoard.Server.Presentation/Program.cs
@@ -7,41 +7,33 @@ using PolyBoard.Server.Presentation.OptionsSetup;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
 //Swagger setup
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "PolyBoard API", Version = "v1" });
-    c.AddSecurityDefinition("cookieAuth", new OpenApiSecurityScheme
-    {
-        Name = ".AspNetCore.Identity.Application",
-        Type = SecuritySchemeType.Http,
-        In = ParameterLocation.Cookie,
-        Description = "ASP.NET Core Identity Cookie"
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "cookieAuth"
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
 });
+
 builder.Services.AddSignalR();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer();
 builder.Services.ConfigureOptions<JwtOptionsSetup>();
 builder.Services.ConfigureOptions<JwtBearerOptionsSetup>();
+
+//Enable Controllers
+builder.Services.AddControllers()
+    //dont wrap response with $id and $values
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", b =>
+        b.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 
 builder.Services.AddApplication()
     .AddInfrastructure(builder.Configuration);
@@ -59,6 +51,11 @@ app.MapHub<LobbyHub>("/lobby");
 
 //TODO
 //app.UseHttpsRedirection();
+
+//Not secure for production!
+app.UseCors("AllowAll");
+app.UseRouting();
+
 app.UseAuthentication();
 app.UseAuthorization();
 
