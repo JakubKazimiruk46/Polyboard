a87c7f74f11ec36dfd636b0e70b13ea49ad5f371
Patryk Zadykowicz <patrykzadykowiczali@gmail.com>
2024-12-01 03:15:53 +0100

    Dodanie obwodu pola przy nakierowaniu na niego myszką. Dodanie wyświetlania karty pola po kliknięciu na pole Dodanie komunikatu o możliwości wyświetlenia detali pola

--- polyboard.client/scenes/board/level/Field.cs
+++ polyboard.client/scenes/board/level/Field.cs
@@ -8,8 +8,11 @@ public partial class Field : Node3D
 	protected Vector3 bRCL; //dolny prawy rog - lokalna pozycja
 	protected Vector3 bRCG; //dolny prawy rog - globalna pozycja
 	public List<Vector3> positions=new List<Vector3>(6);
+	private Sprite3D _border;
+	private Area3D _area;
 	protected static int nextId = 0;
 	public int FieldId;
+	private Sprite2D viewDetailsDialog;
 
 	public Field()
 	{
@@ -18,13 +21,28 @@ public partial class Field : Node3D
 	}
 	public override void _Ready()
 	{
+		
+		viewDetailsDialog = GetNodeOrNull<Sprite2D>("/root/Level/CanvasLayer/TextureRect/ViewDetailsDialog");
+		if (viewDetailsDialog == null)
+	{
+		GD.PrintErr("Błąd: Nie znaleziono Sprite2D do wyświetlania dialogu wyswietlenia detali.");
+	}
 		SetPositions();
 		
 	}
 	public virtual void SetPositions()
 	{
 		fieldMeshInstance = GetNode<MeshInstance3D>("MeshInstance3D");
-		
+		_border = GetNode<Sprite3D>("Border");
+		_area = GetNode<Area3D>("Area3D");
+		if(_border==null || _area==null)
+		{
+			GD.PrintErr("Nie znaleziono obiektow do wyswietlania krawedzi pola ");
+		}
+		_border.Visible = false;
+		_area.Connect("mouse_entered", new Callable(this, nameof(OnMouseEntered))); 
+		_area.Connect("mouse_exited", new Callable(this, nameof(OnMouseExited)));
+		_area.Connect("input_event", new Callable(this, nameof(OnInputEvent)));
 		if (fieldMeshInstance != null)
 		{
 			
@@ -78,6 +96,59 @@ public partial class Field : Node3D
 				
 		}
 	}
+	 private void OnInputEvent(Node camera, InputEvent @event, Vector3 position, Vector3 normal, int shape_idx) 
+	{ 
+		if (@event is InputEventMouseButton mouseButton)
+		{
+		if (mouseButton.Pressed && mouseButton.ButtonIndex ==MouseButton.Left)
+		{
+			var parent = GetParent() as Board;
+			parent?.ShowFieldTexture(FieldId);
+		}
+		}
+		else if (@event is InputEventMouseMotion) 
+	{ 
+		ShowDetailsDialog();
+		_border.Visible = true; 
+	} else if (@event is InputEventMouseButton) 
+	{ 
+		_border.Visible = false;
+	}
+	
+	}
+	
+	public void ShowDetailsDialog()
+	{
+		Texture2D detailsTexture = ResourceLoader.Load<Texture2D>("res://scenes/board/level/textures/ViewDetailsDialog.png");
+		viewDetailsDialog.Texture=detailsTexture;
+		
+		Vector2 viewportSize = GetViewport().GetVisibleRect().Size;
+			
+		float scaleFactorX = viewportSize.X / 2500f;  
+		float scaleFactorY = viewportSize.Y / 1080f;  
+		float scaleFactor = Math.Min(scaleFactorX, scaleFactorY);
+		Vector2 scale = new Vector2(scaleFactor, scaleFactor);
+			
+		viewDetailsDialog.Scale = scale; 
+			
+		viewDetailsDialog.Visible = true; 
+		GD.Print($"viewDetailsDialog Position: {viewDetailsDialog.Position}, Scale: {viewDetailsDialog.Scale}");		
+	}
+	
+	private void OnMouseEntered()
+	 { 
+		_border.Visible = true;
+		 
+		 }
+		 private void OnMouseExited()
+		 { 
+			_border.Visible = false;
+			viewDetailsDialog.Visible=false;
+		 }
+	
+
+
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
