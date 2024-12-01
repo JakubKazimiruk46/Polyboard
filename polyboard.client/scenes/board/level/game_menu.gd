8f03e0bbaaec9540bace641997bf3fb9f9745b1f
dawid <dawiddavtyan16@gmail.com>
2024-12-01 16:44:05 +0100

    wprowadzenie okienek z potwierdzeniem

--- polyboard.client/scenes/board/level/game_menu.gd
+++ polyboard.client/scenes/board/level/game_menu.gd
@@ -1,14 +1,20 @@
 extends Panel
 @onready var game_settings = $"../GameSettings"
+@onready var exit_mainmenu_confirm_window = $"../ExitMainMenuConfirm"
+@onready var exit_game_confirm_window = $"../ExitGameConfirm"
 
 func _on_MainMenuButton_pressed():
-	get_tree().change_scene_to_file("res://scenes/main.menu/main_menu.tscn")
+	self.visible = false
+	exit_mainmenu_confirm_window.set_process(true)
+	exit_mainmenu_confirm_window.visible = true
 
 func _on_ResumeButton_pressed():
 	get_parent().visible = false
 
 func _on_QuitButton_pressed():
-	get_tree().quit()  # Wyjście z gry
+	self.visible = false
+	exit_game_confirm_window.set_process(true)
+	exit_game_confirm_window.visible = true
 
 func _on_OptionButton_pressed():
 	self.visible = false
@@ -19,9 +25,19 @@ func on_exit_settings_menu():
 	self.visible = true
 	game_settings.visible = false
 
+func on_exit_confirm_menu():
+	self.visible = true
+	exit_mainmenu_confirm_window.visible = false
+
+func on_exit_confirm_game():
+	self.visible = true
+	exit_game_confirm_window.visible = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	game_settings.exit_game_settings_menu.connect(on_exit_settings_menu)
+	exit_mainmenu_confirm_window.exit_confirm_menu.connect(on_exit_confirm_menu)
+	exit_game_confirm_window.exit_confirm_game.connect(on_exit_confirm_game)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
