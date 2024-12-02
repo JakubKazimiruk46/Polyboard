6064da927d6f95b1aab53a9ebddc34bcc9a35119
JakubKazimiruk46 <154755194+JakubKazimiruk46@users.noreply.github.com>
2024-12-02 14:59:11 +0100

    Podstawowa logika
    Zostało dodać pobieranie ID

--- polyboard.client/scenes/account/account.gd
+++ polyboard.client/scenes/account/account.gd
@@ -2,17 +2,24 @@ class_name Account
 extends Control
 
 @onready var back_button = $MarginContainer/back_button as Button
-@onready var username_label = $ScrollContainer/VboxContainer/username as LineEdit
+@onready var username = $ScrollContainer/VboxContainer/username as LineEdit
 @onready var email = $ScrollContainer/VboxContainer/email as LineEdit
 @onready var current_pass = $ScrollContainer/VboxContainer/current_password as LineEdit
 @onready var new_pass = $ScrollContainer/VboxContainer/new_password as LineEdit
 @onready var confirm_new_pass = $ScrollContainer/VboxContainer/confirm_new_password as LineEdit
+@onready var save_button = $ScrollContainer/VBoxContainer/save_button as Button
+@onready var http_request = $HTTPRequest as HTTPRequest
 
 signal exit_account_menu()
 
 func _ready():
 	handle_connecting_signals()
 	set_process(false)
+
+func _on_save_pressed() -> void:
+	if username.text.strip() != "":
+		
+	
 
 func on_back_button_pressed() -> void:
 	exit_account_menu.emit()
