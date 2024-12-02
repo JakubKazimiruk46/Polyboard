761081e473bd3e0138225252b9b398875350584b
JakubKazimiruk46 <154755194+JakubKazimiruk46@users.noreply.github.com>
2024-12-02 14:30:41 +0100

    initial

--- polyboard.client/scenes/account/account.gd
+++ polyboard.client/scenes/account/account.gd
@@ -2,6 +2,11 @@ class_name Account
 extends Control
 
 @onready var back_button = $MarginContainer/back_button as Button
+@onready var username_label = $ScrollContainer/VboxContainer/username as LineEdit
+@onready var email = $ScrollContainer/VboxContainer/email as LineEdit
+@onready var current_pass = $ScrollContainer/VboxContainer/current_password as LineEdit
+@onready var new_pass = $ScrollContainer/VboxContainer/new_password as LineEdit
+@onready var confirm_new_pass = $ScrollContainer/VboxContainer/confirm_new_password as LineEdit
 
 signal exit_account_menu()
 
