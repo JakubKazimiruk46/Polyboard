9d295d725caf79ad6f8b3165c3191e43bae30700
Patryk Zadykowicz <patrykzadykowiczali@gmail.com>
2024-12-29 16:58:52 +0100

    Dodanie funkcji budowania domków

--- polyboard.client/scenes/board/level/Field.cs
+++ polyboard.client/scenes/board/level/Field.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 public partial class Field : Node3D
 {
@@ -9,6 +10,10 @@ public partial class Field : Node3D
 	protected Vector3 bRCL; // dolny prawy rog - lokalna pozycja
 	protected Vector3 bRCG; // dolny prawy rog - globalna pozycja
 	public List<Vector3> positions = new List<Vector3>(6);
+	public List<Vector3> buildPositions=new List<Vector3>(5);
+	public List<bool> occupied= new List<bool>(6);
+	public List<bool> buildOccupied=new List<bool>(5);
+	public Vector3 buildCameraPosition;
 	protected Sprite3D _border;
 	protected Area3D _area;
 	protected static int nextId = 0;
@@ -16,6 +21,7 @@ public partial class Field : Node3D
 	public string Name; // Dodano nazwę pola
 	public int ECTSReward = 0; // Ilość ECTS przyznawana za lądowanie na tym polu
 	protected Sprite2D viewDetailsDialog;
+	protected AudioStreamPlayer3D constructionSoundPlayer;
 
 	public Field()
 	{
@@ -24,11 +30,21 @@ public partial class Field : Node3D
 
 		// Ustawienie nazwy pola na podstawie FieldId
 		Name = $"Field{FieldId}";
+		
+		for (int i = 0; i < 6; i++)
+	{
+		occupied.Add(false);
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		buildOccupied.Add(false);
+	}
 	}
 
 	public override void _Ready()
 	{
 		viewDetailsDialog = GetNodeOrNull<Sprite2D>("/root/Level/CanvasLayer/TextureRect/ViewDetailsDialog");
+		constructionSoundPlayer = GetNodeOrNull<AudioStreamPlayer3D>("/root/Level/Board/ConstructionSound");
 		if (viewDetailsDialog == null)
 		{
 			GD.PrintErr("Błąd: Nie znaleziono Sprite2D do wyświetlania dialogu wyswietlenia detali.");
@@ -73,6 +89,12 @@ public partial class Field : Node3D
 				positions.Add(new Vector3(bRCG.X + 1.5f, 0.5f, bRCG.Z - 0.5f));
 				positions.Add(new Vector3(bRCG.X + 0.5f, 0.5f, bRCG.Z - 1.5f));
 				positions.Add(new Vector3(bRCG.X + 0.5f, 0.5f, bRCG.Z - 0.5f));
+				buildPositions.Add(new Vector3(bRCG.X+3.6f, 0.28f, bRCG.Z-0.3f));
+				buildPositions.Add(new Vector3(bRCG.X+3.6f, 0.28f, bRCG.Z-0.8f));
+				buildPositions.Add(new Vector3(bRCG.X+3.6f, 0.28f, bRCG.Z-1.3f));
+				buildPositions.Add(new Vector3(bRCG.X+3.6f, 0.28f, bRCG.Z-1.8f));
+				buildPositions.Add(new Vector3(bRCG.X+3.65f, 0.28f, bRCG.Z-1.0f));
+				buildCameraPosition=new Vector3(bRCG.X-1.0f,0.8f,bRCG.Z-1.0f);
 			}
 			else if (FieldId >= 11 && FieldId <= 19)
 			{
@@ -82,6 +104,12 @@ public partial class Field : Node3D
 				positions.Add(new Vector3(bRCG.X + 0.5f, 0.5f, bRCG.Z + 1.5f));
 				positions.Add(new Vector3(bRCG.X + 1.5f, 0.5f, bRCG.Z + 0.5f));
 				positions.Add(new Vector3(bRCG.X + 0.5f, 0.5f, bRCG.Z + 0.5f));
+				buildPositions.Add(new Vector3(bRCG.X+0.3f, 0.28f, bRCG.Z+3.55f));
+				buildPositions.Add(new Vector3(bRCG.X+0.8f, 0.28f, bRCG.Z+3.55f));
+				buildPositions.Add(new Vector3(bRCG.X+1.3f, 0.28f, bRCG.Z+3.55f));
+				buildPositions.Add(new Vector3(bRCG.X+1.8f, 0.28f, bRCG.Z+3.55f));
+				buildPositions.Add(new Vector3(bRCG.X+1.0f, 0.28f, bRCG.Z+3.65f));
+				buildCameraPosition=new Vector3(bRCG.X+1.0f,0.8f,bRCG.Z-1.0f);
 			}
 			else if (FieldId >= 21 && FieldId <= 30)
 			{
@@ -91,6 +119,12 @@ public partial class Field : Node3D
 				positions.Add(new Vector3(bRCG.X - 1.5f, 0.5f, bRCG.Z + 0.5f));
 				positions.Add(new Vector3(bRCG.X - 0.5f, 0.5f, bRCG.Z + 1.5f));
 				positions.Add(new Vector3(bRCG.X - 0.5f, 0.5f, bRCG.Z + 0.5f));
+				buildPositions.Add(new Vector3(bRCG.X-3.55f, 0.28f, bRCG.Z+0.3f));
+				buildPositions.Add(new Vector3(bRCG.X-3.55f, 0.28f, bRCG.Z+1.8f));
+				buildPositions.Add(new Vector3(bRCG.X-3.55f, 0.28f, bRCG.Z+1.3f));
+				buildPositions.Add(new Vector3(bRCG.X-3.55f, 0.28f, bRCG.Z+1.8f));
+				buildPositions.Add(new Vector3(bRCG.X-3.65f, 0.28f, bRCG.Z+1.0f));
+				buildCameraPosition=new Vector3(bRCG.X+1.0f,0.8f,bRCG.Z+1.0f);
 			}
 			else if (FieldId >= 31 && FieldId <= 40)
 			{
@@ -100,6 +134,12 @@ public partial class Field : Node3D
 				positions.Add(new Vector3(bRCG.X - 0.5f, 0.5f, bRCG.Z - 1.5f));
 				positions.Add(new Vector3(bRCG.X - 1.5f, 0.5f, bRCG.Z - 0.5f));
 				positions.Add(new Vector3(bRCG.X - 0.5f, 0.5f, bRCG.Z - 0.5f));
+				buildPositions.Add(new Vector3(bRCG.X-0.3f, 0.28f, bRCG.Z-3.6f));
+				buildPositions.Add(new Vector3(bRCG.X-0.8f, 0.28f, bRCG.Z-3.6f));
+				buildPositions.Add(new Vector3(bRCG.X-1.3f, 0.28f, bRCG.Z-3.6f));
+				buildPositions.Add(new Vector3(bRCG.X-1.8f, 0.28f, bRCG.Z-3.6f));
+				buildPositions.Add(new Vector3(bRCG.X-1.0f, 0.28f, bRCG.Z-3.65f));
+				buildCameraPosition=new Vector3(bRCG.X-1.0f,0.8f,bRCG.Z+1.0f);
 			}
 		}
 	}
@@ -126,6 +166,85 @@ public partial class Field : Node3D
 		}
 	}
 
+	public async Task BuildHouse(int FieldId)
+{
+	HashSet<int> invalidFieldIds = new HashSet<int> { 0, 2, 4, 5, 7, 10, 12, 15, 17, 20, 22, 25, 28, 30, 33, 35, 36, 38 };
+	if (invalidFieldIds.Contains(FieldId))
+	{
+		return;
+	}
+
+	var houseScene = GD.Load<PackedScene>("res://scenes/board/buildings/house.tscn");
+	var puffScene = GD.Load<PackedScene>("res://scenes/board/buildings/puff.tscn");
+	if (houseScene == null)
+	{
+		GD.PrintErr("Nie udało się załadować sceny domu.");
+		return;
+	}
+	if (puffScene == null)
+	{
+		GD.PrintErr("Nie udało się załadować sceny efektu");
+		return;
+	}
+
+	var homeInstance = houseScene.Instantiate<Node3D>();
+	var puffInstance = puffScene.Instantiate<Node3D>();
+	if (homeInstance != null && puffInstance != null)
+	{
+		homeInstance.RotationDegrees = new Vector3(0, -270, 0);
+		homeInstance.Scale = new Vector3(0.01f, 0.01f, 0.01f);
+		Vector3 defaultHouseScale = new Vector3(0.5f, 0.5f, 0.5f);
+		AddChild(homeInstance);
+		AddChild(puffInstance);
+		homeInstance.GlobalPosition = buildPositions[0];
+		puffInstance.GlobalPosition = buildPositions[0];
+		puffInstance.Scale = new Vector3(1.0f, 1.0f, 1.0f);
+
+		Timer timer = new Timer();
+		timer.WaitTime = 3.0f;
+		timer.OneShot = true;
+		GetTree().Root.AddChild(timer);
+
+		var buildCamera = new Camera3D();
+		GetTree().Root.AddChild(buildCamera);
+		buildCamera.GlobalPosition = buildCameraPosition;
+		buildCamera.LookAt(buildPositions[0], Vector3.Up);
+		buildCamera.Current = true;
+
+		timer.Start();
+		PlayConstructionSound();
+		Tween tween = CreateTween();
+		tween.TweenProperty(homeInstance, "scale", defaultHouseScale, 1.5f)
+			 .SetTrans(Tween.TransitionType.Linear)
+			 .SetEase(Tween.EaseType.InOut);
+
+		
+		await ToSignal(tween, "finished");
+		await ToSignal(timer, "timeout");
+		StopConstructionSound();
+		
+		RemoveChild(puffInstance);
+		puffInstance.QueueFree();
+		
+		timer.WaitTime=1.5f;
+		timer.Start();
+		await ToSignal(timer, "timeout");
+		
+		
+		
+		RemoveChild(buildCamera);
+		buildCamera.QueueFree();
+		return;
+	}
+	else
+	{
+		GD.PrintErr("Nie udało się stworzyć sceny domku.");
+		return;
+	}
+}
+
+	
+
 	public void ShowDetailsDialog()
 	{
 		Texture2D detailsTexture = ResourceLoader.Load<Texture2D>("res://scenes/board/level/textures/ViewDetailsDialog.png");
@@ -148,6 +267,28 @@ public partial class Field : Node3D
 		_border.Visible = true;
 	}
 
+private void PlayConstructionSound()
+	{
+		if (constructionSoundPlayer != null)
+		{
+			constructionSoundPlayer.Play();
+			GD.Print("Odtwarzanie dźwięku budowania.");
+		}
+		else
+		{
+			GD.PrintErr("Błąd: AudioStreamPlayer3D nie jest zainicjalizowany.");
+		}
+	}
+
+	private void StopConstructionSound()
+	{
+		if (constructionSoundPlayer != null && constructionSoundPlayer.Playing)
+		{
+			constructionSoundPlayer.Stop();
+			GD.Print("Zatrzymanie dźwięku budowania.");
+		}
+	}
+	
 	protected void OnMouseExited()
 	{
 		if (!isMouseEventEnabled) return;
